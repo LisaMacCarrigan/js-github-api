d01a94a2b2ce8a421fb9cd417d990495ed037144
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-10-14 12:18:52 -0400

    add functioning search that returns object from API

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,25 +1,23 @@
 var Search = require('./../js/search.js').searchModule;
 
-var displaySearchResults = function(repoData){
-  $('#displayed-search-result').text(repoData);
-};
+// var displaySearchResults = function(repoData){
+//   $('#displayed-search-result').text(repoData);
+// };
 
 //  ========================== Search =========================== //
 
 $(document).ready(function(){
 
-var currentSearchObject = new Search();
+  var currentSearch = new Search();
 
-$('#username-search-form').submit(function(event){
-  event.preventDefault();
-  var username = $('#username').val();
+  $('#username-search-form').submit(function(event){
+    event.preventDefault();
+    var username = $('#username').val();
 
-  $('#displayed-username').text(' "' + username + '"');
-  $('#search-result-section').show();
-}); // end submit
+    $('#displayed-username').text(' "' + username + '"');
+    $('#search-result-section').show();
+    currentSearch.getRepos(username);
 
-//  =======================Display Results ======================= //
-
-$('#display-repo-description')
+  }); // end submit
 
 }); //end ready
--- js/search.js
+++ js/search.js
@@ -3,14 +3,14 @@ var apiKey = require('./../.env').apiKey;
 function Search(){
 }
 
-Search.prototype.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+Search.prototype.getRepos = function(username){
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
-
 
 
 exports.searchModule = Search;
