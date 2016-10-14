ab7839a19a5cc15a37ab6f70c4c2b27eca359557
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-10-14 13:39:36 -0400

    add function for displaying repositories

--- js/search-interface.js
+++ js/search-interface.js
@@ -7,15 +7,10 @@ var Search = require('./../js/search.js').searchModule;
 //  ========================== Search =========================== //
 
 $(document).ready(function(){
-
   var currentSearch = new Search();
-
   $('#username-search-form').submit(function(event){
     event.preventDefault();
     var username = $('#username').val();
-
     currentSearch.getRepos(username);
-
   }); // end submit
-
 }); //end ready
--- js/search.js
+++ js/search.js
@@ -6,12 +6,24 @@ function Search(){
 Search.prototype.getRepos = function(username){
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
-    $('#displayed-username').text(' "' + username + '"');
-    $('#search-result-section').show();
+    $('#displayed-username').text(response.login);
     $('#no-results-for-username').text("");
+    $('.list-group').empty();
+    $('#search-result-section').show();
+
   }).fail(function(error){
     $('#no-results-for-username').text("Username " + error.responseJSON.message);
     $('#search-result-section').hide();
+  });
+
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+    console.log(response);
+
+    for (var repo = 0; repo < response.length; repo++){
+      $(".list-group").append("<li id='displayed-repository-name' class='list-group-item'>" + response[repo].name + "</li>")
+    }
+  }).fail(function(error){
+    $(".list-group").append("<li id='displayed-repository-name' class='list-group-item'>" + error.responseJSON.message + "</li>");
   });
 };
 
