d41c942c5e5042406f66dace2f924eef333fb70e
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-10-14 13:59:29 -0400

    modify for loop to display repositories - test looping through user name

--- js/search.js
+++ js/search.js
@@ -6,24 +6,24 @@ function Search(){
 Search.prototype.getRepos = function(username){
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
-    $('#displayed-username').text(response.login);
+    $('#displayed-username').text(" " + response.login);
     $('#no-results-for-username').text("");
-    $('.list-group').empty();
-    $('#search-result-section').show();
 
   }).fail(function(error){
-    $('#no-results-for-username').text("Username " + error.responseJSON.message);
+    $('#no-results-for-username').text("User " + error.responseJSON.message);
     $('#search-result-section').hide();
   });
 
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
+    $('#search-result-section').show();
 
     for (var repo = 0; repo < response.length; repo++){
-      $(".list-group").append("<li id='displayed-repository-name' class='list-group-item'>" + response[repo].name + "</li>")
+      console.log(response[repo].login);
+      $(".list-group").append("<li class='list-group-item'>" + response[repo].login + "</li>");
     }
+
   }).fail(function(error){
-    $(".list-group").append("<li id='displayed-repository-name' class='list-group-item'>" + error.responseJSON.message + "</li>");
+    console.log(error.responseJSON.message);
   });
 };
 
